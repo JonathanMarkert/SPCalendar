b5ae9b31c796afabadd68e4a8b4d53cc280d20fa
Alpha-85 <ljungdahlt@gmail.com>
2021-06-20 12:39:42 +0200

    Co-authored-by: DanielSundin <DanielSundin@users.noreply.github.com> Co-authored-by: JohannaSkinnari <jskinnari@gmail.com> Co-authored-by: Jonathan Markert <JonathanMarkert@users.noreply.github.com>

--- js/todo.js
+++ js/todo.js
@@ -1,1 +1,23 @@
-// todo js
+
+function initTodos() {
+    addEventListeners();
+}
+
+function addEventListeners() {
+    const createButton = document.querySelector('.createButton');
+    createButton.addEventListener('click', openCreateNewTodoForm);
+}
+
+
+function openCreateNewTodoForm() {
+    console.log("click!")
+    
+    // gömma en div och visa en annan ? 
+}
+
+
+
+
+
+
+
