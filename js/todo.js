b1874f71fe2c7c4fa787448a34e47fa77569fdbf
Johanna Skinnari <jskinnari@gmail.com>
2021-06-20 12:39:42 +0200

    fixed so plus button and avbryt button shows and hides divs. JS DS TL JM

--- js/todo.js
+++ js/todo.js
@@ -6,13 +6,26 @@ function initTodos() {
 function addEventListeners() {
     const createButton = document.querySelector('.createButton');
     createButton.addEventListener('click', openCreateNewTodoForm);
+    
+    const exitButton = document.querySelector('.exitButton');
+    exitButton.addEventListener('click', closeCreateNewTodoForm);
 }
 
 
 function openCreateNewTodoForm() {
-    console.log("click!")
-    
-    // gömma en div och visa en annan ? 
+    let todoForm = document.querySelector('#sidebar-todo-form');
+    let sidebarWrapper = document.querySelector('#sidebar-wrapper');
+
+    sidebarWrapper.setAttribute('class', 'd-none');
+    todoForm.classList.remove('d-none');
+}
+
+function closeCreateNewTodoForm() {
+    let todoForm = document.querySelector('#sidebar-todo-form');
+    let sidebarWrapper = document.querySelector('#sidebar-wrapper');
+
+    todoForm.setAttribute('class', 'd-none');
+    sidebarWrapper.classList.remove('d-none');
 }
 
 
